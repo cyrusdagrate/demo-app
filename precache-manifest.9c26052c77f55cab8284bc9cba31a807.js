d4dc9a267880671c537bd7ae7c24a38b85e8cc0f
Kody Kaftousian <you@example.com>
2020-03-18 16:08:59 -0500

    Updates

---
+++ precache-manifest.9c26052c77f55cab8284bc9cba31a807.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8e153fd606ab340352f9c44f72b449",
+    "url": "/demo-app/index.html"
+  },
+  {
+    "revision": "2a0e1d89969c10c2682d",
+    "url": "/demo-app/static/css/main.ed7673b0.chunk.css"
+  },
+  {
+    "revision": "d84bc95a12b92578aa28",
+    "url": "/demo-app/static/js/2.c7b85ca2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/demo-app/static/js/2.c7b85ca2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a0e1d89969c10c2682d",
+    "url": "/demo-app/static/js/main.5fc9661a.chunk.js"
+  },
+  {
+    "revision": "bc28c24b86477ce62ed9",
+    "url": "/demo-app/static/js/runtime-main.5c842d52.js"
+  },
+  {
+    "revision": "12a6659ff5467c71b599841286fb8e7f",
+    "url": "/demo-app/static/media/logo.12a6659f.svg"
+  }
+]);
